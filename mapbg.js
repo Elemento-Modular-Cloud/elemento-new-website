2bf617052ecf8723cc74eec6172b3c569f39048f
Gabriele Fronzé <sucre.91@hotmail.it>
2021-09-19 12:18:06 +0200

    Fixing overflow

--- mapbg.js
+++ mapbg.js
@@ -188,8 +188,12 @@ function init() {
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
+    console.log("Div found")
+
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
+
+    console.log("Map created")
 }
 
 $(document).ready(function(){
