8fbe20a16492233def5f8004b995d4110036e184
Gabriele Fronzé <sucre.91@hotmail.it>
2021-09-20 15:00:12 +0200

    Embedding mailchimp subscribe form

---
+++ mcsub-form.js
@@ -0,0 +1,33 @@
+var email = document.getElementById('email')
+
+new Mcsub('#form-wrapper-id', {
+    user:  '4f312c8c16810c3c59ec2d0d6',
+    list:  '4a0560ee16',
+    callback:  'cb',
+    reponse:  '#response',
+    onInit(){
+        console.log(this.element) // Example
+        email.addEventListener('keydown', function() {
+            button.classList.add('is-active');
+        });
+        this.button.addEventListener('click', function(e){
+            this.classList.add('is-done','is-active');
+            
+            setTimeout(function(){ 
+              button.innerHTML = "Email registered!"
+            }, 500);
+          });
+    },
+    onSubmit(){
+        console.log(this.button); // Example
+    },
+    onSuccess(){
+        console.log('Subscribe Sucess'); // Example
+    },
+    onError(){
+        console.log('Subscribe Error'); // Example
+    },
+    complete(){
+        console.log(this.inputs); // Example
+    }
+});
