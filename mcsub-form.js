99b010310f1b61a498c7f3e01d9f64ded07a1588
Gabriele Fronzé <sucre.91@hotmail.it>
2021-09-22 01:21:57 +0200

    Making form detect invalid emails

--- mcsub-form.js
+++ mcsub-form.js
@@ -1,6 +1,15 @@
 var email = document.getElementById('email')
 var label = document.getElementById('email-label')
 
+function validateEmail(mail) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(mail).toLowerCase());
+}
+
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
 new Mcsub('#mailchimpform', {
     user:  '4f312c8c16810c3c59ec2d0d6',
     list:  '4a0560ee16',
@@ -13,11 +22,24 @@ new Mcsub('#mailchimpform', {
             label.classList.add('min-label')
         });
         this.button.addEventListener('click', function(e){
-            this.classList.add('is-done','is-active');
-            
-            setTimeout(function(){ 
-              button.innerHTML = "Email registered!"
-            }, 500);
+            console.log(email.value)
+            if(validateEmail(email.value)){
+                this.classList.add('is-done','is-active');
+                
+                setTimeout(function(){ 
+                button.innerHTML = String(email.value).toLowerCase()+" registered!"
+                }, 500);
+            } else {
+                this.classList.remove('is-done');
+                this.classList.add('is-invalid');
+
+                button.innerHTML = "Invalid email!"
+
+                sleep(3000).then(() => {
+                    button.innerHTML = "Submit";
+                    this.classList.remove('is-invalid');
+                });
+            }
           });
     },
     onSubmit(){
