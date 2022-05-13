b501410f7b0c5300043a57e9130888045e3ae193
Gabriele Fronzé <sucre.91@hotmail.it>
2022-05-14 01:44:09 +0200

    WIP slider

--- funnel.js
+++ funnel.js
@@ -1,6 +1,9 @@
 const discover_page = document.getElementById('discover').classList;
 
 function toggleDiscover() {
-    console.log("Called")
     discover_page.toggle("visible-scroller-page")
+}
+
+function closeDiscover() {
+    discover_page.remove("visible-scroller-page")
 }
